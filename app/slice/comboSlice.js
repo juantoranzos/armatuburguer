421ffea55997a9670da68e2f2df0f5cc1829a892
Juanchi <101830727+juantoranzos@users.noreply.github.com>
2025-07-22 21:03:58 -0300

    fix client components and redux setup

--- app/slice/comboSlice.js
+++ app/slice/comboSlice.js
@@ -1,21 +1,25 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
- const initialState = [{
-    items:[],
-    loading: false
- }]
+
+const initialState = {
+  items: [],
+  loading: false,
+};
 export const fetchCombos = createAsyncThunk("combos/fetchCombos", async (combo)=>{
     const resp = await fetch("/api/combos");
     return resp.json()
 })
 
-export const añadirCombo = createAsyncThunk("combos/añadirCombo", async (combo)=>{
+export const añadirCombo = createAsyncThunk(
+  "combos/añadirCombo",
+  async (combo) => {
     const resp = await fetch("/api/combos", {
-        method: "POST",
-        headers: { "Content-Type": "application.json"},
-        body: JSON.stringify(combo)
-    })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(combo),
+    });
     return resp.json();
-} )
+  }
+);
 const comboSlice = createSlice({
     name: "combos",
     initialState: initialState,
